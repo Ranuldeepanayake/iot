c96d2cb63b46814969b8c48d5738b2f9dcdac221
Ranga Perera <71638813+RangaPerera94@users.noreply.github.com>
2022-08-28 23:03:11 +0530

    UI merged

--- Software/Web App/server.js
+++ Software/Web App/server.js
@@ -12,7 +12,7 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 //app.use(express.json());
-app.use(express.static(__dirname + '/agriSmart/'));	//Use Ranga's static resources.
+app.use(express.static(__dirname + '/ejs/'));	//Use Ranga's static resources.
 app.set('view engine', 'ejs')
 
 //Path preparation.
